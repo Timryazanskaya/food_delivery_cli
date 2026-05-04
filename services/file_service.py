1e24d9f2c26a96c1bf6f42c8d0dabb175a93b9c9
Timryazanskaya <mary.tim2007@gmail.com>
2026-05-04 21:37:23 +0400

    add auto-creation of data files if not exist

--- services/file_service.py
+++ services/file_service.py
@@ -2,8 +2,13 @@ import json
 import os
 
 def load_data(filename):
+    import os
+    import json
+
     if not os.path.exists(filename):
-        return []
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump([], f)
+
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
 
